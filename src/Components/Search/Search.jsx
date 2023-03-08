ef6dc32cd20895542ff8d263c5d8d45256236a79
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-08 12:27:30 +0100

    Started working on the Search Component

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Search.css";
 
 const Submit = () => {};
 const Search = () => {
+  const [img, setImg] = useState("");
+  const [res, setRes] = useState([]);
   return (
     <div className="search-container">
-      <input type="text" placeholder="Search Anything..." />
+      <input
+        type="text"
+        placeholder="Search Anything..."
+        value={img}
+        onChange={(e) => setImg(e.target.value)}
+      />
       <button type="submit" onClick={Submit}>
         Search
       </button>
