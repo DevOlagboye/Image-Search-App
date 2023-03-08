e8b6a16fc0089a59e3f073d6e1d8c62cd7790374
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-08 12:16:44 +0100

    Started working on the Search Component

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./Search.css";
 
+const Submit = () => {};
 const Search = () => {
-  <div className="search-container">
-    <input type="text" placeholder="Search Anything..." />
-  </div>;
+  return (
+    <div className="search-container">
+      <input type="text" placeholder="Search Anything..." />
+      <button type="submit" onClick={Submit}>
+        Search
+      </button>
+    </div>
+  );
 };
 export default Search;
