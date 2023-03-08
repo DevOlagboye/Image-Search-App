876e9dd6a4e9b469cda8afab03a59ca26b9235c1
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-08 13:15:12 +0100

    Finished consuming the Unsplash API to fetch images related to what the user searches

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./Search.css";
 
-const Submit = () => {};
 const Search = () => {
   const [img, setImg] = useState("");
   const [res, setRes] = useState([]);
+  const fetchRequest = async () => {
+    const data = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+    );
+    const dataJ = await data.json();
+    const result = dataJ.results;
+    console.log(result);
+    setRes(result);
+  };
+  useEffect(() => {
+    fetchRequest();
+  }, []);
+  const Submit = () => {
+    fetchRequest();
+    setImg("");
+  };
   return (
     <div className="search-container">
       <input
@@ -16,6 +31,18 @@ const Search = () => {
       <button type="submit" onClick={Submit}>
         Search
       </button>
+
+      <div className="image-container">
+        {res.map((val) => {
+          return (
+            <img
+              className="images"
+              src={val.urls.small}
+              alt={val.alt_description}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
