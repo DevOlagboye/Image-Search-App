3028f7570b951a438b41b217338ac6f8e92393ce
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-08 12:11:26 +0100

    Added the Search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Search from './Components/Search/Search';
+
 
 function App() {
   return (
     <div className="App">
-      
+    <Search/>
     </div>
   );
 }
